0bb4407722877505e0fb42c9a15304a3604c009c
aniketSaha1997 <67219239+aniketSaha1997@users.noreply.github.com>
2023-01-25 18:01:52 +0100

    Add files via upload

---
+++ DJI_Configuration/command.py
@@ -0,0 +1,56 @@
+# -*- encoding: utf-8 -*-
+# Test environment: Python 3.6
+
+import socket
+import sys
+
+# In networking connection mode, the current IP address of the robot is 192.168.0.115 and the control command port is port 40923.
+# Replace the IP address of the robot with the actual one. 192.168.224.242', 41332
+host = "172.20.10.3"
+port = 40923
+
+def main():
+
+        address = (host, int(port))
+
+        # Establish a TCP connection with the control command port of the robot.
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        print("Connecting...")
+
+        s.connect(address)
+
+        print("Connected!")
+
+        while True:
+
+                # Wait for the user to enter control commands.
+                msg = input(">>> please input SDK cmd: ")
+
+                # When the user enters Q or q, exit the current program.
+                if msg.upper() == 'Q':
+                        break
+
+                # Add the ending character.
+                msg += ';'
+
+                # Send control commands to the robot.
+                s.send(msg.encode('utf-8'))
+
+                try:
+                        # Wait for the robot to return the execution result.
+                        buf = s.recv(1024)
+
+                        print(buf.decode('utf-8'))
+                except socket.error as e:
+                        print("Error receiving :", e)
+                        sys.exit(1)
+                if not len(buf):
+                        break
+
+        # Disconnect the port connection.
+        s.shutdown(socket.SHUT_WR)
+        s.close()
+
+if __name__ == '__main__':
+        main()
